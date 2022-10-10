94b69f85d2110a3cc2fab027603ac7ffecbee347
Iz9chela-Prac <105288851+Iz9chela-Practice@users.noreply.github.com>
2022-10-10 12:59:51 +0200

    Add files via upload

---
+++ moduleOne.mjs
@@ -0,0 +1,7 @@
+let one = 1
+let two = 'two'
+
+const mul = (a,b) => a * b
+
+const sum = (a, b) => a+b
+export {one, two, sum, mul}
---
+++ moduleTwo.mjs
@@ -0,0 +1,12 @@
+// import sumNumbers from "./moduleOne.mjs";
+import{
+    one as oneNumber,
+    two,
+    sum as sumNumbers,
+    mul
+} from "./moduleOne.mjs"
+
+console.log(oneNumber)
+console.log(two)
+console.log(sumNumbers(1,4))
+console.log(mul(4,5))
